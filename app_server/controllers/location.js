77d4b9eb199b4c2a405eef62b8de09f74222df15
anveshreddy098 <anveshreddymalla@gmail.com>
2023-09-12 13:51:16 +0530

    add more css

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -1,10 +1,10 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('locations-list', { title: 'Home' });
    };
    /* GET 'Location info' page */
 const locationInfo = (req, res) => {
-    res.render('index', { title: 'Location info' });
+    res.render('location-info', { title: 'Location info' });
    };
    /* GET 'Add review' page */
 const addReview = (req, res) => {
